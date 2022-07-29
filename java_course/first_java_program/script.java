cba6e878a353c70399514cb5a88069f9509b2d91
laith isbaitan <laith.isbaitan@axsos.me>
2022-07-29 13:39:34 +0300

    java first program

---
+++ java_course/first_java_program/script.java
@@ -0,0 +1,9 @@
+package java_course.first_java_program;
+
+public class script {
+    public static void main(String[] args) {
+        System.out.println("My name is laith");
+        System.out.println("I am 20 years old");
+        System.out.println("my hometown is ramallah");
+    }
+}
